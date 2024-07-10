03386edd2481264b61da4f8aefd6d9a08677160f
Max Rohrhirsch <max007istcool@gmail.com>
2024-07-10 08:00:53 +0200

    bis Aktionsseite

--- server.js
+++ server.js
@@ -53,64 +53,6 @@ fs.readdirSync("./views/aktionen").forEach(year => {
   });
 });
 
-app.get("/login", (req, res) => {
-  req.session.redirect = req.query.red;
-
-  if (req.session.loggedIn) {
-    res.redirect("/Pfadfinder");
-  } else {
-    res.render("login.ejs", {
-      w_usr: false,
-      usr: "",
-      w_pwd: false,
-      pwd: "",
-      remember: false,
-    });
-  }
-});
-
-app.post("/login", async (req, res) => {
-  const {username, password, remember} = req.body;
-
-  if (!(username && password)) {
-    return res.render("login.ejs", {
-      w_usr: !username,
-      usr: username,
-      w_pwd: !password,
-      pwd: password,
-      remember: remember,
-    });
-  }
-  const user = users.find(x => x.username === username);
-  if (!user) {
-    return res.render("login.ejs", {
-      w_usr: true,
-      usr: username,
-      w_pwd: false,
-      pwd: password,
-      remember: remember,
-    });
-  }
-
-  if (await bcrypt.compare(password, user.password)) {
-    req.session.loggedIn = true;
-    return res.redirect(req.session.redirect);
-  }
-  else {
-    return res.render("login.ejs", {
-      w_usr: false,
-      usr: username,
-      w_pwd: true,
-      pwd: password,
-      remember: remember,
-    });
-  }
-});
-
-app.post("/logout", (req, res) => {
-  req.session.destroy();
-  res.redirect("back");
-});
 
 app.use((req, res) => {
   res.sendFile(path.resolve() + "/views/static/errorpage.html");
